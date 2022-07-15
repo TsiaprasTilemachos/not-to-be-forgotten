9a00785f6d6d5b53c3e198255071a3949ceca786
Tsiapras Tilemachos <33802644+TsiaprasTilemachos@users.noreply.github.com>
2022-07-15 16:25:48 +0300

    Update myUtilities.py

--- myUtilities.py
+++ myUtilities.py
@@ -19,3 +19,31 @@ def float_to_q31(flt):
     if neg:
         fix = fix | 0x80000000
     return fix;
+
+def Hex(num) :
+    m = dict.fromkeys(range(16), 0);
+    digit = ord('0')
+    c = ord('a')
+    for i in range(16) :
+        if (i < 10) :
+            m[i] = chr(digit)
+            digit += 1
+        
+        else :
+            m[i] = chr(c)
+            c += 1
+    res = ""
+    if (not num) :
+        return "0"
+
+    if (num > 0) :
+        while (num) :
+            res = m[num % 16] + res
+            num //= 16
+    else :
+        n = num + 2**16
+        while (n) :
+            res = m[n % 16] + res
+            n //= 16
+
+    return res
